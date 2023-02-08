38ab453818f4bfed92cf47c5223a759466cec053
hyeona kim <>
2023-02-08 21:11:25 +0900

    04.filter로 아이템 삭제하기

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,25 @@
 import FoodList from "./FoodListItem";
-import items from "../mock.json";
+import mockitems from "../mock.json";
 import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState(mockitems);
   const [order, setOrder] = useState("createdAt");
   const sortedItemList = items.sort((a, b) => b[order] - a[order]);
 
   const handleLastClick = () => setOrder("createdAt");
   const handleCaloriClick = () => setOrder("calorie");
+
+  const handleDelete = (id) => {
+    const nextItems = items.filter((item) => item.id !== id);
+    setItems(nextItems);
+  };
+
   return (
     <div>
       <button onClick={handleLastClick}>최신순</button>
       <button onClick={handleCaloriClick}>칼로리순</button>
-      <FoodList items={sortedItemList}></FoodList>
+      <FoodList items={sortedItemList} onDelete={handleDelete}></FoodList>
     </div>
   );
 }
--- src/components/FoodListItem.js
+++ src/components/FoodListItem.js
@@ -1,23 +1,24 @@
-function FoodListItem({ item }) {
+function FoodListItem({ item, onDelete }) {
   const { imgUrl, title, calorie, content } = item;
-
+  const handleDeleteClick = () => onDelete(item.id);
   return (
     <div>
       <img src={imgUrl} alt={title}></img>
       <div>{title}</div>
       <div>{calorie}</div>
       <div>{content}</div>
+      <button onClick={handleDeleteClick}>삭제</button>
     </div>
   );
 }
 
-function FoodList({ items }) {
+function FoodList({ items, onDelete }) {
   return (
     <ul>
       {items.map((item) => {
         return (
           <li>
-            <FoodListItem item={item}></FoodListItem>
+            <FoodListItem item={item} onDelete={onDelete}></FoodListItem>
           </li>
         );
       })}
