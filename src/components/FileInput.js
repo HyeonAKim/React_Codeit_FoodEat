87cd432f699c75bd7d999a0a438300ff0d46fa91
hyeona kim <foren4012@gmail.com>
2023-03-03 23:21:45 +0900

    17.미리보기 생성하기

--- src/components/FileInput.js
+++ src/components/FileInput.js
@@ -1,7 +1,8 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function FileInput({ name, value, onChange }) {
   const inputRef = useRef();
+  const [preview, setPreview] = useState();
 
   const handleChange = (e) => {
     console.log(e.target.files[0]);
@@ -16,8 +17,22 @@ function FileInput({ name, value, onChange }) {
     inputNode.value = "";
     onChange(name, null);
   };
+
+  useEffect(() => {
+    if (!value) return;
+
+    const nextPreview = URL.createObjectURL(value);
+    setPreview(nextPreview);
+
+    return () => {
+      setPreview();
+      URL.revokeObjectURL(nextPreview);
+    };
+  }, [value]);
+
   return (
     <div>
+      <img src={preview} alt="이미지 미리보기"></img>
       <input type="file" onChange={handleChange} ref={inputRef} />
       {value && <button onClick={handleClearClick}>X</button>}
     </div>
